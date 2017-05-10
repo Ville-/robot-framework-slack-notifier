e9fdaac7e608cdc5f08141a26640bd8171d483c6
Ville Saarinen <vmjs123@gmail.com>
2017-05-10 16:39:13 +0300

    remove empty attachments from message

--- RobotFrameworkSlackNotifier.py
+++ RobotFrameworkSlackNotifier.py
@@ -11,10 +11,9 @@ class RobotFrameworkSlackNotifier:
     NON_CRITICAL_INDEX = 1;
     FAIL_INDEX = 2;
 
-    def __init__(self, slack_id, channel='#general', show_documentation=True):
+    def __init__(self, slack_id, show_documentation=True):
         self.url = self.SLACK_URL_ROOT + slack_id
         self.show_documentation = show_documentation
-        self.channel = channel
 
         self._init_slack_message()
 
@@ -22,14 +21,10 @@ class RobotFrameworkSlackNotifier:
         self.slack_message = {}
         self.slack_message['unfurl_links'] = False
         self.slack_message['mrkdwn'] = True
-        self.slack_message['channel'] = self.channel
         self.slack_message['username'] = "Robot Framework Notifier"
         self.slack_message['icon_emoji'] = ":robot_face:"
 
         self.slack_message['attachments'] = [{}, {}, {}]
-        self._init_attachment(self.PASS_INDEX, 'Passed', 'good')
-        self._init_attachment(self.NON_CRITICAL_INDEX, 'Non-Critical', 'warning')
-        self._init_attachment(self.FAIL_INDEX, 'Failed', 'danger')
 
     def _init_attachment(self, index, title, color):
         self.slack_message['attachments'][index]['fields'] = []
@@ -41,10 +36,17 @@ class RobotFrameworkSlackNotifier:
 
         if result['status'] == 'PASS':
             test_result = self.slack_message['attachments'][self.PASS_INDEX]
+            if not 'fields' in test_result:
+                self._init_attachment(self.PASS_INDEX, 'Passed', 'good')
+
         elif result['critical'] == 'no':
             test_result = self.slack_message['attachments'][self.NON_CRITICAL_INDEX]
+            if not 'fields' in test_result:
+                self._init_attachment(self.NON_CRITICAL_INDEX, 'Passed', 'good')
         else:
             test_result = self.slack_message['attachments'][self.FAIL_INDEX]
+            if 'fields' in test_result:
+                self._init_attachment(self.FAIL_INDEX, 'Passed', 'good')
 
         test_result['fields'].append({
             "title": name,
@@ -58,5 +60,6 @@ class RobotFrameworkSlackNotifier:
             self.slack_message['text'] +=  "\n" + result['doc']
 
         requests.post(self.url, data=json.dumps(self.slack_message))
+
 
 
